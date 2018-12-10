77b984acd637fde65168954946c0f1478003bba2
Erik Bauer <erik_bauer@hotmail.com>
2018-12-10 13:18:11 +0100

    modified the network

--- conv.py
+++ conv.py
@@ -10,25 +10,33 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 from keras import backend as K
-K.set_image_dim_ordering('th')
+
+import pickle
+
+K.set_image_dim_ordering('tf')
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
 #load data etc
+X = pickle.load(open("features.pickle", "rb"))
+Y = pickle.load(open("labels.pickle", "rb"))
+print(X.shape[1:])
+
+Y = np_utils.to_categorical(Y)
 
 #define the larger model
 def larger_model():
     #Create model
     model=Sequential()
-    model.add(Conv2D(30,(5,5), input_shape(1, 28, 28), activation='relu'))
+    model.add(Conv2D(30,(5,5), input_shape=X.shape[1:], activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Conv2D(15,(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(50,activation='relu'))
+    model.add(Dense(5,activation='relu'))
     #Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
@@ -36,3 +44,4 @@ def larger_model():
 #build the model
 model=larger_model();
 #fit the model
+model.fit(X, Y, batch_size=32, epochs=3, validation_split=0.1)
